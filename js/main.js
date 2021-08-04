30744d2e99212ba7d220b8ff797e9a6085c0d6be
kate <strelkate94@gmail.com>
2021-08-04 22:33:59 +0500

    Create: grid, section news

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
+$(document).ready(function () {
+    var tabsItem = $(".main-screen__tab");
+    var contentItem = $(".main-screen__left");
+    var contentItem0 = $(".main-screen__left-0");
+
+    tabsItem.on("click", function (event) {
+        var activeContent = $(this).attr("data-target");
+        tabsItem.removeClass('main-screen__tab-active');
+        contentItem.removeClass('main-screen__left-active');
+        // contentItem0.removeClass('main-screen__left-0');
+        $(activeContent).toggleClass('main-screen__left-active');
+        $(this).toggleClass('main-screen__tab-active');
+    });
+});
 var menuButton = $(".menu-button");
 menuButton.on('click', function () {
-    $('.navbar-menu').toggleClass('navbar-menu--visible')
-})
+    $('.navbar-menu').toggleClass('navbar-menu--visible');
+});
