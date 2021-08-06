760c12894fbb6e86a4ed383390bbd7ee2e57cd06
kate <strelkate94@gmail.com>
2021-08-06 20:24:59 +0500

    Create: added contact__form, footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Перееключение табов
 $(document).ready(function () {
     var tabsItem = $(".main-screen__tab");
     var contentItem = $(".main-screen__left");
@@ -12,7 +13,27 @@ $(document).ready(function () {
         $(this).toggleClass('main-screen__tab-active');
     });
 });
+
+//Мобильное меню
 var menuButton = $(".menu-button");
 menuButton.on('click', function () {
     $('.navbar-menu').toggleClass('navbar-menu--visible');
+});
+
+//Скачивание файла
+$('.grid__bottom-download').on('click', function(){
+    var link = document.createElement('a');
+    link.setAttribute('href', 'img/smiley-face.jpeg');
+    link.setAttribute('download', 'smiley-face.jpeg');
+    link.click();
+    return false;
+});
+
+//Смена цвета у bookmark
+$(".list__icon-bookmark").on('click', function (e) {
+    if( e.currentTarget.style.fill !== "rgb(255, 79, 82)") {
+        e.currentTarget.style.fill = "#FF4F52";
+    } else{
+        e.currentTarget.style.fill = "#BCBFC2";
+    }
 });
